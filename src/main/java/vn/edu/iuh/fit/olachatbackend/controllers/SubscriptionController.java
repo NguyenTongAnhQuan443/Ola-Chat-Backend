609e654a8715cebc36156199a6935db3a6a13b55
Nguyễn Tống Anh Quân <97744835+NguyenTongAnhQuan443@users.noreply.github.com>
2025-05-23 20:07:16 +0700

    Feature/update etc (#100)
    * Resolve conflict merge dev into feature/UpdateEtc
    
    * Update dependence
    
    * Update gitignore
    
    * Update API cancel friend request
    
    * Fix API list of received friend requests
    
    * Update API sent friend request. friend request can only be sent after 24 hours if rejected
    
    * Update API get my notification
    
    * Fix endpoint notification public
    
    * Baothong (#67)
    
    * Tạo một bài đăng mới
    
    * Cập nhật tạo một bài đăng mới. Xem bài đăng
    
    * Xóa bài đăng
    
    * Update Xóa bài đăng
    
    * API Cập nhật bài đăng
    
    * API Thích bài đăng
    
    * API Hủy thích bài đăng
    
    * API Thêm bình luận vào bài đăng
    
    * API Lấy danh sách bình luận của bài đăng
    
    * API Xóa bình luận của bài đăng
    
    * API Trả lời bình luận của bài đăng, cấu trúc lại bình luận trả về có phân cấp
    
    * API Cập nhật bình luận của bài đăng
    
    * API Share Post, cập nhật API xóa và API chỉnh sửa Post
    
    * Cập nhật API getAllPosts thành getUserPosts, cập nhật getPostById
    
    * Feature/group (#69)
    
    * update messageDTO
    
    * create function auto create conversation and update lastmessage, notify after accept friend request
    
    * update last message
    
    * update api get conversation by user
    
    * Baothong (#68)
    
    * Tạo một bài đăng mới
    
    * Cập nhật tạo một bài đăng mới. Xem bài đăng
    
    * Xóa bài đăng
    
    * Update Xóa bài đăng
    
    * API Cập nhật bài đăng
    
    * API Thích bài đăng
    
    * API Hủy thích bài đăng
    
    * API Thêm bình luận vào bài đăng
    
    * API Lấy danh sách bình luận của bài đăng
    
    * API Xóa bình luận của bài đăng
    
    * API Trả lời bình luận của bài đăng, cấu trúc lại bình luận trả về có phân cấp
    
    * API Cập nhật bình luận của bài đăng
    
    * API Share Post, cập nhật API xóa và API chỉnh sửa Post
    
    * Cập nhật API getAllPosts thành getUserPosts, cập nhật getPostById
    
    * Cập nhật API download, chọn savePath để lưu
    
    * Fix error font UTF-8 FE
    
    * Add API delete conversation by id
    
    * Update etc
    
    * Add field isFriend to search user
    
    * Update response API search user
    
    * Update API register accout fix error exiting email
    
    * Fix return if API error token
    
    * Update securityconfig
    
    * Add Dockerfile and Jenkinsfile
    
    * Update jenkinfile
    
    * Update jenkinfile
    
    * Update jenkinfile
    
    * Firebase to deploy backend
    
    * Update Firebase and jenkinfile and dockerfile
    
    * Update Firebase and jenkinfile and dockerfile
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Add API Weather
    
    * Add API Weather
    
    * Update APi weather
    
    ---------
    
    Co-authored-by: Đặng Bảo Thông <122684823+baothong2k3@users.noreply.github.com>
    Co-authored-by: Nguyễn Thanh Nhứt <101404518+NguyenThanhNhut13@users.noreply.github.com>

--- src/main/java/vn/edu/iuh/fit/olachatbackend/controllers/SubscriptionController.java
+++ src/main/java/vn/edu/iuh/fit/olachatbackend/controllers/SubscriptionController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import vn.edu.iuh.fit.olachatbackend.models.Subscriber;
 import vn.edu.iuh.fit.olachatbackend.repositories.SubscriberRepository;
+import vn.edu.iuh.fit.olachatbackend.services.EmailService_Weather;
 
 import java.util.Optional;
 
@@ -14,6 +15,8 @@ public class SubscriptionController {
 
     @Autowired
     private SubscriberRepository subscriberRepo;
+    @Autowired
+    private EmailService_Weather emailService_Weather;
 
     @PostMapping
     public ResponseEntity<?> subscribe(@RequestBody Subscriber s) {
@@ -21,6 +24,15 @@ public class SubscriptionController {
             return ResponseEntity.badRequest().body("Email already registered!");
         }
         subscriberRepo.save(s);
+        String content = "Chúc mừng bạn đã đăng ký nhận thông báo thời tiết thành công!\n"
+                + "Bạn sẽ nhận bản tin thời tiết vào 7h sáng hàng ngày.\n"
+                + "Nếu muốn huỷ, hãy sử dụng chức năng hủy đăng ký trên ứng dụng/web.";
+
+        emailService_Weather.sendWeatherEmail(
+                s.getEmail(),
+                "Xác nhận đăng ký nhận thông báo thời tiết",
+                content
+        );
         return ResponseEntity.ok("Registered successfully!");
     }
 
